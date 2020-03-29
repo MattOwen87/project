fef73db059da2a4558c2731eb35e3282a88f0995
Matthew Owen Haworth <matthew_haworth@hotmail.co.uk>
2020-03-29 14:37:43 +0100

    Added Image Interventio Composer Dependancies

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Intervention\Image\Facades\Image;
 
 class ProfileController extends Controller
 {
